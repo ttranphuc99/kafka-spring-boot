d4d1564afa619c7a6d0b1045de95aca5b78923c0
Tran Phuc <ttran.phuc99@gmail.com>
2022-10-09 17:34:03 +0700

    init prj

---
+++ src/main/java/com/phuctt/kafkalab/KafkalabApplication.java
@@ -0,0 +1,13 @@
+package com.phuctt.kafkalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkalabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkalabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phuctt/kafkalab/KafkalabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuctt.kafkalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkalabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
